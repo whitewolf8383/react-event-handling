b0ad43326c13182a3d871e62448d03b231fb2416
whitewolf8383 <kandt.vandf@gmail.com>
2021-08-28 15:54:09 -0600

    Basic form handling

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
-  return <h1>Hello World</h1>
+  const [name, setName] = useState("");
+  const [headingText, setHeadingText] = useState("");
+
+  function updateHeading(event) {
+    setName(event.target.value);
+  }
+  function handleClick(event) {
+    setHeadingText(name);
+  }
+
+  return (
+    <div>
+      <h1>Hello {headingText}</h1>
+      <input 
+        onChange={updateHeading} 
+        type="text" 
+        placeholder="What's your name?" 
+        value={name}
+      />
+      <button onClick={handleClick}>Submit</button>
+    </div>
+  );
 }
 
 export default App;
